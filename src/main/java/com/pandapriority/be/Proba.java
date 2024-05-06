d482a2a74394fcd675b49406119ed2d7f814c9d8
dzejlaomerhodzic <dzejla.omerhodzic@stu.ssst.edu.ba>
2024-05-06 17:55:09 +0200

    Update: deleted

---
+++ src/main/java/com/pandapriority/be/Proba.java
@@ -0,0 +1,4 @@
+package com.pandapriority.be;
+
+public class Proba {
+}
--- src/main/java/com/pandapriority/be/TaskManagementService.java
+++ src/main/java/com/pandapriority/be/TaskManagementService.java
@@ -1,6 +1,5 @@
 package com.pandapriority.be;
 
-import com.pandapriority.be.Task;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
