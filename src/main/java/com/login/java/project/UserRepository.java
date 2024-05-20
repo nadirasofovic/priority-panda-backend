71f327d5999af6b3f3b9139a32bc9bca2c897bb9
Ena Frasto <ena.321@icloud.com>
2024-05-20 11:04:33 +0200

    Add this

---
+++ src/main/java/com/login/java/project/UserRepository.java
@@ -0,0 +1,7 @@
+package com.login.java.project;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
