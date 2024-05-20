f02bdf567b6a188ef3278705e40997f99571019c
hanadigaribovic <hanadi.garibovic@2gimnazija.edu.ba>
2024-05-20 11:31:28 +0200

    Table for Tasks

---
+++ src/main/java/com/login/java/project/Task.java
@@ -0,0 +1,42 @@
+package com.login.java.project;
+
+import javax.persistence.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.util.List;
+
+@Table(name = "tasks")
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+    public class Task {
+        @Id
+        @GeneratedValue
+        private long id;
+
+        @Column(name = "title")
+        private String title;
+
+        @Column(name = "description")
+        private String description;
+
+        @Column(name = "dueDate")
+        private String dueDate;
+
+        @Column(name = "priority")
+        private String priority;
+
+        @Column(name = "label")
+        private String label;
+    }
+
+
+
+}
---
+++ src/main/java/com/login/java/project/TaskManagmentController.java
@@ -0,0 +1,37 @@
+package com.login.java.project;
+
+public class TaskManagmentController {
+    import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.util.List;
+
+    @Table(name = "workspace")
+    @Entity
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public class Workspace {
+        @Id
+        @GeneratedValue
+        private long id;
+
+        @Column(name = "title")
+        private String title;
+
+        @Column(name = "description")
+        private String description;
+
+        @Column(name = "dueDate")
+        private String dueDate;
+
+        @ElementCollection
+        @CollectionTable(name = "workspace_members", joinColumns = @JoinColumn(name = "workspace_id"))
+        @Column(name = "members")
+        private List<String> members;
+
+    }
+}
